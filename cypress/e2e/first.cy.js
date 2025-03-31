a2e9af44169e8f341499114e67225c48fde8bd89
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-03-31 16:32:45 -0500

    Added cypress lines to delete a camper's info

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,7 +1,7 @@
 describe('Fill out form and Check List', () => {
   it('Has filled out the form and checked the list', () => {
     cy.visit('http://127.0.0.1:5000/')
-    cy.get('input[name="name"]').type('Snowy Thompson', {delay: 100})
+    cy.get('input[name="name"]').type('Snowy Thompson', {delay: 100}) //create camper
     cy.get('input[name="age"]').type('12')
     cy.get('input[name="number"]').type('601-630-8004', {delay: 100})
     cy.get('input[name="email"]').type('clevelandfan13@gmail.com', {delay: 100})
@@ -9,5 +9,9 @@ describe('Fill out form and Check List', () => {
     cy.get('input[name="tshirt"]').type('Extra Small', {delay: 100})
     cy.get('form').submit()
     cy.get('a[href="/list"]').click()
+    cy.get('a[href="/delete"]').click() 
+    cy.get('input[name]').type('snowy thompson')//assuring that this is case sensetive
+    cy.get('form').submit()
+    cy.get('a[href="/"]').click()
   })
 })
