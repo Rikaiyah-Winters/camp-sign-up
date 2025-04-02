0364ec3c78ff1f31f0660ae4175b99819f166a60
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2025-04-02 13:55:53 -0500

    Updated name in first.cy cypress test

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,16 +1,16 @@
 describe('Fill out form and Check List', () => {
   it('Has filled out the form and checked the list', () => {
     cy.visit('http://127.0.0.1:5000/')
-    cy.get('input[name="name"]').type('Snowy Thompson', {delay: 100}) //create camper
+    cy.get('input[name="name"]').type('Snowy Simpson', {delay: 100}) //create camper
     cy.get('input[name="age"]').type('12')
     cy.get('input[name="number"]').type('601-630-8004', {delay: 100})
-    cy.get('input[name="email"]').type('clevelandfan13@gmail.com', {delay: 100})
+    cy.get('input[name="email"]').type('coltsfan13@gmail.com', {delay: 100})
     cy.get('input[name="address"]').type('123 Virgo Rd, Calabasas, LA, CA 90210', {delay: 100})
     cy.get('input[name="tshirt"]').type('Extra Small', {delay: 100})
     cy.get('form').submit()
     cy.get('a[href="/list"]').click()
     cy.get('a[href="/delete"]').click() 
-    cy.get('input[name]').type('snowy thompson')//assuring that this is case sensetive
+    cy.get('input[name]').type('snowy simpson')//assuring that this is case sensetive
     cy.get('form').submit()
     cy.get('a[href="/"]').click()
   })
